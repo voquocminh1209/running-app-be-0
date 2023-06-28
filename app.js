d652e6f9723080e6a7b33eabe4cbad0d5b933cb1
voquocminh1209 <voquocminh1209@gmail.com>
2023-06-28 20:36:31 +0700

    fix: .env

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-const express = require('express')
-const app = express()
-const mongoose = require("mongoose")
-const morgan=require('morgan')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 // để xem log
 app.use(morgan("tiny"));
 
-//Using enviroment variable 
+//Using enviroment variable
 require("dotenv").config();
-
 
 //middleware
 app.use(express.urlencoded()); // Parse URL-encoded bodies
 app.use(express.json()); //to support JSON encode
 
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors());
 
-var userRouter = require("./router/userRouter")
-app.use("/api/users",userRouter);
+var userRouter = require("./router/userRouter");
+app.use("/api/users", userRouter);
 
-var foodRouter = require("./router/foodRouter")
-app.use("/api/food",foodRouter);
+var foodRouter = require("./router/foodRouter");
+app.use("/api/food", foodRouter);
 
-var planRouter = require("./router/planRouter")
-app.use("/api/plan",planRouter);
+var planRouter = require("./router/planRouter");
+app.use("/api/plan", planRouter);
 
-var activitiesRouter = require("./router/activitiesRouter")
-app.use("/api/activities",activitiesRouter);
+var activitiesRouter = require("./router/activitiesRouter");
+app.use("/api/activities", activitiesRouter);
 
-const connectString=process.env.connectString;
+const DB_URI = process.env.DB_URI;
 // kết nối database
 mongoose
-  .connect(connectString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((value) => {
     console.log("database connect success");
   })
@@ -46,6 +45,3 @@ var server = app.listen(PORT, function () {
   var port = server.address().port;
   console.log("Express is working on port", port);
 });
-
-
-
